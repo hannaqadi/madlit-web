6c381d45131190e96cebb05c6fed9881a2ad2029
Hanna <hannaabdulalim@gmail.com>
2025-01-15 16:51:27 -0800

    fixed playing page error

--- src/Playing.js
+++ src/Playing.js
@@ -4,10 +4,28 @@ import { useBlocker } from 'react-router-dom'
 
 const Playing = () => {
   const navigate = useNavigate();
-  const story = localStorage.getItem('story')
+  const [story, setStory] = useState(null);
   const [inputs, setInputs] = useState({});
-  const isSubmittingRef = useRef(false); 
+  const [loaded, setLoaded] = useState(false)
+  const isSubmittingRef = useRef(false);
   const isDirtyRef = useRef(false)
+
+  useEffect(()=>{
+    try {
+      const storyData = localStorage.getItem("story");
+      console.log(storyData)
+      if (storyData) {
+        const parsedStory = JSON.parse(storyData);
+        setStory(parsedStory); 
+        setLoaded(true);
+      } else {
+        setLoaded(false); 
+      }
+    } catch (error) {
+      console.error("Error parsing story from localStorage:", error);
+      setLoaded(false);
+    }
+  },[])
 
   const handleChange = (index, event) => {
     const { value } = event.target
@@ -15,7 +33,7 @@ const Playing = () => {
       ...prevInputs,
       [index]: value
     }))
-  
+
     isDirtyRef.current = true;
   }
 
@@ -34,7 +52,7 @@ const Playing = () => {
     event.preventDefault();
     isSubmittingRef.current = true;
     isDirtyRef.current = false;
-    
+
     let finalInputs = []
     for (const value in inputs) {
       finalInputs.push(inputs[value])
@@ -47,32 +65,35 @@ const Playing = () => {
     })
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     isSubmittingRef.current = false;
     isDirtyRef.current = false;
   }, [])
- 
+
   return (
     <div>
-      <h3>Playing</h3>
-      <p>{story.title}</p>
-      <p>hello?</p>
-      <Prompt when={isDirtyRef.current} message='Are you sure you want to leave?' />
-      <div>
-        <form onSubmit={handleSubmit}>
-          {JSON.parse(story).partOfSpeech.map((part, index) => (
-            <div key={index}>
-              <p>{part}</p>
-              <input
-                type="text"
-                value={inputs[index] || ""}
-                onChange={(e) => handleChange(index, e)}
-              />
-            </div>
-          ))}
-          <button>Submit</button>
-        </form>
-      </div>
+      {loaded ? <div>
+        <h3>Playing</h3>
+        <p>{story.title}</p>
+        <p>hello?</p>
+        <Prompt when={isDirtyRef.current} message='Are you sure you want to leave?' />
+        <div>
+          <form onSubmit={handleSubmit}>
+            {story.parts_of_speech.map((part, index) => (
+              <div key={index}>
+                <p>{part}</p>
+                <input
+                  type="text"
+                  value={inputs[index] || ""}
+                  onChange={(e) => handleChange(index, e)}
+                />
+              </div>
+            ))}
+            <button>Submit</button>
+          </form>
+        </div>
+      </div> : <p>oops!</p>}
+
     </div>
   )
 }
